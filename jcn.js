ecf7b0876b56eabdd7c69cf08830c55ac389436d
bhautik302ofc <bhautik.shish@gmail.com>
2024-07-31 11:34:21 +0530

    Update jcn.js

--- jcn.js
+++ jcn.js
@@ -1,6 +1,6 @@
 var amont = "true"; 
 const provider = "@ybl";
-const upiname = "mr--sakariya";
+const upiname = "Q312913865";
 const upid = upiname + provider ;
 // const upid = "mr-sakariya@ybl";
 const adminname = "DonateFor";
